d656c6284516c7aa884a778b678c36753fae572b
Jon Cain <jonifen@users.noreply.github.com>
2020-01-15 22:06:06 +0000

    rebuilding site 15 Jan 2020 22:06:04

---
+++ js/header-bar.js
@@ -0,0 +1,10 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("#header").style.top = "0";
+  } else {
+    document.querySelector("#header").style.top = window.innerWidth > 840 ? "-3rem" : "-6rem";
+  }
+  prevScrollpos = currentScrollPos;
+}
